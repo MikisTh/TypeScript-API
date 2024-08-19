fb407293bb42cdb0b4122365a56e3f38897e4cb5
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-19 11:03:53 -0300

    Update InsertSort.ts
    Add Insert

--- InsertSort.ts
+++ InsertSort.ts
@@ -15,3 +15,17 @@ class InsertionSort
        }
    }
 }
+
+function insertionSort(arr: number[]): number[] {
+  const n = arr.length;
+  for (let i = 1; i < n; i++) {
+    const currentElement = arr[i];
+    let j = i - 1;   
+    while (j >= 0 && arr[j] > currentElement) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentElement;
+  }
+  return arr;
+}
