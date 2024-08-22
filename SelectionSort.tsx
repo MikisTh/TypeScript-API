f560eff65996f0658ee9a326be11a97c58215711
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-22 11:07:33 -0300

    Update SelectionSort.tsx
    Add

--- SelectionSort.tsx
+++ SelectionSort.tsx
@@ -19,3 +19,27 @@ class SelectionSort {
 const sorter = new SelectionSort();
 const sortedArray = sorter.sort([64, 25, 12, 22, 11]);
 console.log(sortedArray); // Output: [11, 12, 22, 25, 64]
+
+class SelectionSort
+{
+   public sort(arr: number[])
+   {
+       for(let i = 0; i < arr.length - 1; i++)
+       {
+           let min = i;
+           
+           for(let j = i + 1; j < arr.length; j++)
+           {
+               if(arr[j] < arr[min])
+               {
+                   // Choose the lesser of the two:
+                   min = j;
+               }
+           }
+           // In-place swap:
+           const tmp = arr[min];
+           arr[min] = arr[i];
+           arr[i] = tmp;
+       }
+   }
+}
